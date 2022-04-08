3d39375573b395a71bf12857c55bfa50f134f791
Yuri-Stack <yurioliveirasilva1999@gmail.com>
2022-04-08 09:29:20 -0300

    Códigos de Link entre Usuário e Postagem

---
+++ 6 - aula-postagem-usuario/src/models/User.ts
@@ -0,0 +1,10 @@
+// Altere a Model conforme a Model do Back
+interface User{
+    id: number
+    nome: string
+    usuario: string
+    senha: string
+    foto: string    // Add o campo Foto
+}
+
+export default User
