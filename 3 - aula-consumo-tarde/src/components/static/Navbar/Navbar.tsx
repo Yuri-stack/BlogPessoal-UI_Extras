051ef61e69e5d8b97876450e7bbab90f902d96f1
Yuri-Stack <yurioliveirasilva1999@gmail.com>
2022-05-02 17:09:54 -0300

    Subindo códigos com as tratativas de erro do Cadastro de Usuario

---
+++ 3 - aula-consumo-tarde/src/components/static/Navbar/Navbar.tsx
@@ -0,0 +1,66 @@
+import React from 'react'
+import { AppBar, Box, Toolbar, Typography } from '@material-ui/core'
+import { Link } from 'react-router-dom'
+
+import './Navbar.css'
+
+function NavBar() {
+    return (
+        <>
+            <AppBar position="static">
+                <Toolbar variant="dense">
+                    <Box className='cursor'>
+                        <Typography variant="h5" color="inherit">
+                            BlogPessoal
+                        </Typography>
+                    </Box>
+
+                    <Box display="flex" justifyContent="start">
+                        <Link to="/home" className="text-decorator-none">
+                            <Box mx={1} className='cursor'>
+                                <Typography variant="h6" color="inherit">
+                                    Home
+                                </Typography>
+                            </Box>
+                        </Link>
+
+                        <Link to="/posts" className="text-decorator-none">
+                            <Box mx={1} className='cursor'>
+                                <Typography variant="h6" color="inherit">
+                                    Postagens
+                                </Typography>
+                            </Box>
+                        </Link>
+
+                        <Link to="/temas" className="text-decorator-none">
+                            <Box mx={1} className='cursor'>
+                                <Typography variant="h6" color="inherit">
+                                    Temas
+                                </Typography>
+                            </Box>
+                        </Link>
+
+                        <Link to="/formularioTema" className="text-decorator-none">
+                            <Box mx={1} className='cursor'>
+                                <Typography variant="h6" color="inherit">
+                                    Cadastrar Temas
+                                </Typography>
+                            </Box>
+                        </Link>
+
+                        <Link to="/login" className='text-decorator-none'>
+                            <Box mx={1} className='cursor'>
+                                <Typography variant="h6" color="inherit">
+                                    Logout
+                                </Typography>
+                            </Box>
+                        </Link>
+                    </Box>
+
+                </Toolbar>
+            </AppBar>
+        </>
+    )
+}
+
+export default NavBar
