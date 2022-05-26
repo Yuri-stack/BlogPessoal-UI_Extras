7826fa8700fa8fe543eba5acb2654ce3d4762bb8
Yuri-Stack <yurioliveirasilva1999@gmail.com>
2022-05-26 10:14:51 -0300

    Organizando

--- 5 - aula-revisao-carrinho-simples/src/models/Produto.ts
+++
@@ -1,9 +0,0 @@
-interface Produto{
-    id: number
-    nome: string
-    preco: number
-    quantidade: number
-    imagem: string
-}
-
-export default Produto
--- 7 - aula-postagem-usuario/src/react-app-env.d.ts
+++
@@ -1,5 +0,0 @@
-/// <reference types="react-scripts" />
-
-// Tipando as imagens para podemos usar no React
-declare module '*.png'
-declare module '*.jpg'
