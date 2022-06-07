840094e8fac86887db9a5750067d0664857434e8
Yuri-Stack <yurioliveirasilva1999@gmail.com>
2022-06-07 15:55:31 -0300

    Conteudo Perfil

---
+++ 5 - aula-perfil-usuario/src/models/UserLogin.ts
@@ -0,0 +1,11 @@
+// Altere a Model conforme a Model do Back
+interface UserLogin{
+    id: number
+    nome: string
+    usuario: string
+    senha: string
+    foto: string
+    token: string   // Mudamos o parametro do Token para aceitar somente String
+}
+
+export default UserLogin
